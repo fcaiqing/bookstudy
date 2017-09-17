7ec8c9d0b345d6bd19891c339f8e8284a0d7e58a
fcaiqing <fcaiqing@163.com>
2017-09-17 20:46:32 +0800

    9-17

--- eloquent-js/chapter7/index.js
+++ eloquent-js/chapter7/index.js
@@ -35,9 +35,31 @@ function Point(x, y) {
     this.y = y;
 }
 Point.prototype.plus = function (other) {
-
    return new Point(other.x+this.x, other.y+this.y);
 }
 var grid = [["top left", "top middle", "top right"],
             ["bottom left", "bottom middle", "bottom right"]];
 console.log(grid[1][2]);
+
+//
+function Grid(width, height) {
+    this.space = new Array(width*height);
+    this.width = width;
+    this.height = height;
+}
+Grid.prototype.isInside = function (point) {
+    return point.x>=0&&point.x<this.width&&
+    point.y>=0&&point.y<this.height;
+};
+Grid.prototype.get = function (point) {
+    return this.space[point.x+point.y*this.width];
+};
+Grid.prototype.set = function (point, value) {
+    this.space[point.x+point.y*this.width] = value;
+};
+//
+var grid = new Grid (5, 5);
+log(grid.get(new Point(1, 1)));
+grid.set(new Point(1, 1), "X");
+log(grid.get(new Point(1, 1)));
+//
